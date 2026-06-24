080a016e317e1a4e029209a0bdfbfaaed33ad5ff
Pranith Jain <pranithjain2006@gmail.com>
2026-06-24 23:11:30 +0530

    Update newtest.py

--- newtest.py
+++ newtest.py
@@ -1,1 +1,13 @@
+numbers = [10, 20, 30, 40, 50]
 
+sum = 0
+
+for i in range(len(number)):   # Error 1
+    sum += numbers[i]
+
+average = sum / len(numbers)
+
+print("Average is " + average)  # Error 2
+
+if average > 30
+    print("Average is greater than 30")  # Error 3
